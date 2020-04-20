d1d12bb4583b1efe34c08066ea80dbda20839181
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-04-20 19:23:18 +0300

    Microdata in comments, also in comments navigation
    gh-12

--- component/frontend/ViewTemplates/Comments/default.php
+++ component/frontend/ViewTemplates/Comments/default.php
@@ -42,21 +42,21 @@ $paginationData = $this->getPagination()->getData();
 		<?= $this->container->template->loadPosition('engage-after-comments') ?>
 
 		<div class="akengage-pagination">
-			<div class="akengage-pagination-pages">
+			<div class="akengage-pagination-pages" itemscope itemtype="http://www.schema.org/SiteNavigationElement">
 				<?php if (!empty($paginationData->start->link)): ?>
-					<a href="<?= $this->rebasePageLink($paginationData->start->link) ?>">
+					<a itemprop="hasPart" rel="start" href="<?= $this->rebasePageLink($paginationData->start->link) ?>">
 						<span title="<?= $paginationData->start->text ?>">⏮️</span>
 					</a>
 				<?php endif; ?>
 				<?php if (!empty($paginationData->previous->link)): ?>
-					<a href="<?= $this->rebasePageLink($paginationData->previous->link) ?>">
+					<a itemprop="hasPart" rel="prev" href="<?= $this->rebasePageLink($paginationData->previous->link) ?>">
 						<span title="<?= $paginationData->previous->text ?>">⏪</span>
 					</a>
 				<?php endif; ?>
 
 				<?php foreach($paginationData->pages as $page => $def): ?>
 					<?php if (!empty($def->link)): ?>
-						<a href="<?= $this->rebasePageLink($def->link) ?>" class="<?= $def->active ? 'active' : '' ?>"
+						<a itemprop="hasPart" rel="bookmark" href="<?= $this->rebasePageLink($def->link) ?>" class="<?= $def->active ? 'active' : '' ?>"
 						   title="<?= Text::sprintf('COM_ENGAGE_COMMENTS_LBL_PAGE', $def->text) ?>">
 							<?= $def->text ?>
 						</a>
@@ -68,12 +68,12 @@ $paginationData = $this->getPagination()->getData();
 				<?php endforeach; ?>
 
 				<?php if (!empty($paginationData->next->link)): ?>
-					<a href="<?= $this->rebasePageLink($paginationData->next->link) ?>">
+					<a itemprop="hasPart" rel="next" href="<?= $this->rebasePageLink($paginationData->next->link) ?>">
 						<span title="<?= $paginationData->next->text ?>">⏩</span>
 					</a>
 				<?php endif; ?>
 				<?php if (!empty($paginationData->end->link)): ?>
-					<a href="<?= $this->rebasePageLink($paginationData->end->link) ?>">
+					<a itemprop="hasPart" rel="bookmark" href="<?= $this->rebasePageLink($paginationData->end->link) ?>">
 						<span title="<?= $paginationData->end->text ?>">⏭️</span>
 					</a>
 				<?php endif; ?>
