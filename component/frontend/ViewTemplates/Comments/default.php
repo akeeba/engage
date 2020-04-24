14cde123667c908d644849dd701977c6e1769c62
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-04-24 14:10:30 +0300

    Finishing touches
    gh-21
    
    Fix pagination

--- component/frontend/ViewTemplates/Comments/default.php
+++ component/frontend/ViewTemplates/Comments/default.php
@@ -43,41 +43,8 @@ $paginationData = $this->getPagination()->getData();
 
 		<?php if ($this->pagination->pagesTotal > 1): ?>
 		<div class="akengage-pagination">
-			<div class="akengage-pagination-pages" itemscope itemtype="http://www.schema.org/SiteNavigationElement">
-				<?php if (!empty($paginationData->start->link)): ?>
-					<a itemprop="hasPart" rel="start" href="<?= $this->rebasePageLink($paginationData->start->link) ?>">
-						<span title="<?= $paginationData->start->text ?>">⏮️</span>
-					</a>
-				<?php endif; ?>
-				<?php if (!empty($paginationData->previous->link)): ?>
-					<a itemprop="hasPart" rel="prev" href="<?= $this->rebasePageLink($paginationData->previous->link) ?>">
-						<span title="<?= $paginationData->previous->text ?>">⏪</span>
-					</a>
-				<?php endif; ?>
-
-				<?php foreach($paginationData->pages as $page => $def): ?>
-					<?php if (!empty($def->link)): ?>
-						<a itemprop="hasPart" rel="bookmark" href="<?= $this->rebasePageLink($def->link) ?>" class="<?= $def->active ? 'active' : '' ?>"
-						   title="<?= Text::sprintf('COM_ENGAGE_COMMENTS_LBL_PAGE', $def->text) ?>">
-							<?= $def->text ?>
-						</a>
-					<?php else: ?>
-						<span class="active">
-							<?= $def->text ?>
-						</span>
-					<?php endif; ?>
-				<?php endforeach; ?>
-
-				<?php if (!empty($paginationData->next->link)): ?>
-					<a itemprop="hasPart" rel="next" href="<?= $this->rebasePageLink($paginationData->next->link) ?>">
-						<span title="<?= $paginationData->next->text ?>">⏩</span>
-					</a>
-				<?php endif; ?>
-				<?php if (!empty($paginationData->end->link)): ?>
-					<a itemprop="hasPart" rel="bookmark" href="<?= $this->rebasePageLink($paginationData->end->link) ?>">
-						<span title="<?= $paginationData->end->text ?>">⏭️</span>
-					</a>
-				<?php endif; ?>
+			<div class="akengage-pagination-pages pagination" itemscope itemtype="http://www.schema.org/SiteNavigationElement">
+				<?= $this->pagination->getPagesLinks() ?>
 			</div>
 		</div>
 		<?php endif; ?>
