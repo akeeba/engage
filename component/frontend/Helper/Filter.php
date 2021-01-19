859b0bd6deed21b89a9c828e99edc16a7a4a231e
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-01-19 21:10:16 +0200

    ! Security b/c break in Joomla 4.0.0 Beta 6: Joomla input filter properties tagBlacklist and attrBlacklist have been renamed, crippling post filtering under some circumstances.

--- component/frontend/Helper/Filter.php
+++ component/frontend/Helper/Filter.php
@@ -304,8 +304,8 @@ final class Filter
 
 		self::$joomlaFilterSettings = [
 			'filterType'          => 'noHTML',
-			'blacklistTags'       => $filter->tagBlacklist,
-			'blacklistAttributes' => $filter->attrBlacklist,
+			'blacklistTags'       => $filter->tagBlacklist ?? $filter->blockedTags,
+			'blacklistAttributes' => $filter->attrBlacklist ?? $filter->blockedAttributes,
 			'whitelistTags'       => [],
 			'whitelistAttributes' => [],
 		];
