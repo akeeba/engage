5a8f916164f5c80a73b5f2f37967152e5bf16b2b
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-02-24 14:13:35 +0200

    + Yes/No options in the component and plugin options now work correctly under Joomla 4.0 beta 7 and later

---
+++ component/backend/fields/fancyradio.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package   AkeebaEngage
+ * @copyright Copyright (c)2020-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') || die();
+
+require_once JPATH_LIBRARIES . '/fof40/Html/Fields/fancyradio.php';
