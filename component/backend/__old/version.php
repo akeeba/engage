6fa3b980a5f47be8987be283bb5e9d0939ee1c1f
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-11-25 12:59:32 +0200

    Converting to Joomla 4
    Start the process by moving things around

---
+++ component/backend/__old/version.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package   AkeebaEngage
+ * @copyright Copyright (c)2020-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die('Restricted Access');
+
+define('AKENGAGE_PRO', '0');
+define('AKENGAGE_VERSION', '2.0.5');
+define('AKENGAGE_DATE', '2021-10-26');
