144d8a60b1fe94220b84558aa415b0d3ddf2369b
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-11-29 13:09:57 +0200

    Converting to Joomla 4
    Common JavaScript for event handling

---
+++ component/media/js/events.js
@@ -0,0 +1,63 @@
+/*
+ * @package   ats
+ * @copyright Copyright (c)2011-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+"use strict";
+
+window.addEventListener('DOMContentLoaded', function() {
+    var onEventSubmit = function (event)
+    {
+        event.preventDefault();
+
+        var elChangedElement = event.currentTarget;
+        var targetString     = elChangedElement.dataset.engagesubmittarget ?? "";
+        var elTarget         = document.forms.adminForm ? document.forms.adminForm : null;
+
+        if (targetString !== "")
+        {
+            elTarget = document.getElementById(targetString);
+        }
+
+        if (!elTarget)
+        {
+            return true;
+        }
+
+        elTarget.submit();
+
+        return false;
+    };
+
+    var onClickConfirm = function (event)
+    {
+        event.preventDefault();
+
+        var elChangedElement  = event.currentTarget;
+        var confirmLangString = elChangedElement.dataset.engageconfirmmessage ?? "";
+
+        if (confirmLangString === "")
+        {
+            return true;
+        }
+
+        return confirm(Joomla.Text._(confirmLangString));
+    };
+
+    [].slice.call(document.querySelectorAll('.engageCommonEventsOnChangeSubmit')).forEach(function(el) {
+        el.addEventListener('change', onEventSubmit);
+    });
+
+    [].slice.call(document.querySelectorAll('.engageCommonEventsOnClickSubmit')).forEach(function(el) {
+        el.addEventListener('click', onEventSubmit);
+    });
+
+    [].slice.call(document.querySelectorAll('.engageCommonEventsOnBlurSubmit')).forEach(function(el) {
+        el.addEventListener('blur', onEventSubmit);
+    });
+
+    [].slice.call(document.querySelectorAll('.engageCommonEventsOnClickConfirm')).forEach(function(el) {
+        el.addEventListener('click', onClickConfirm);
+    });
+});
