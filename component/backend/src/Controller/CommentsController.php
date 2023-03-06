b87afe1ba2f98caf895a606a08e9bb0edd086e39
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-03-06 08:43:18 +0200

    # [LOW] Mark as spam: success message shown as error
    Close gh-269

--- component/backend/src/Controller/CommentsController.php
+++ component/backend/src/Controller/CommentsController.php
@@ -123,20 +123,26 @@ class CommentsController extends AdminController
 			{
 				$model->publish($cid, -3);
 				$errors = $model->getErrors();
-				$ntext  = null;
 
-				if ($errors)
+				if (empty($errors))
 				{
-					Factory::getApplication()->enqueueMessage(Text::plural($this->text_prefix . '_N_ITEMS_MARKED_POSSIBLE_SPAM', \count($cid)), 'error');
+					Factory::getApplication()
+						->enqueueMessage(
+							Text::plural(
+								$this->text_prefix . '_N_ITEMS_MARKED_POSSIBLE_SPAM',
+								\count($cid)
+							)
+						);
 				}
-				else
+				elseif (\count($cid))
 				{
-					$ntext = $this->text_prefix . '_N_ITEMS_FAILED_MARK_POSSIBLE_SPAM';
-				}
-
-				if (\count($cid))
-				{
-					$this->setMessage(Text::plural($ntext, \count($cid)));
+					$this->setMessage(
+						Text::plural(
+							$this->text_prefix . '_N_ITEMS_FAILED_MARK_POSSIBLE_SPAM',
+							\count($cid)
+						),
+						'error'
+					);
 				}
 			}
 			catch (\Exception $e)
